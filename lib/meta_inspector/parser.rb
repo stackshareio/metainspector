2e7abb2a338ee60fc04e42d9e53123340b2d063a
Muhammet <dilekmuhammet@gmail.com>
2019-06-25 00:06:27 +0300

    delegate to lang

--- lib/meta_inspector/parser.rb
+++ lib/meta_inspector/parser.rb
@@ -26,7 +26,7 @@ module MetaInspector
     delegate [:head_links, :stylesheets, :canonicals, :feed]                               => :@head_links_parser
     delegate [:links, :base_url]                                                           => :@links_parser
     delegate :images                                                                       => :@images_parser
-    delegate [:title, :best_title, :author, :best_author, :description, :best_description] => :@texts_parser
+    delegate [:lang, :title, :best_title, :author, :best_author, :description, :best_description] => :@texts_parser
 
     # Returns the whole parsed document
     def parsed
